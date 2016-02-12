190ac6a415029e3ef664442a20a5a6226aee58a2
Adrian Rudin <adrian@mav.ch>
2016-02-12 17:38:47 +0100

    Adding initial unit test files (based on boost.test)

---
+++ test/libarude_test.hpp
@@ -0,0 +1,32 @@
+///
+/// This file belongs to the libarude.
+///
+/// \author Adrian Rudin
+/// \copyright Copyright 2016 Adrian Rudin (arude).
+///
+/// For commercial or closed source software a commercial license must be
+/// obtained. Please contact me.
+///
+/// This file/project is part of libarude and released under the GNU General
+/// Public License for non commercial software.
+///
+/// libarude is free software for non commerical use. You can redistribute it
+/// and / or modify it under the terms of the GNU General Public License as
+/// published by the Free Software Foundation, either version 3 of the License,
+/// or (at your option) any later version.
+///
+/// libarude is distributed in the hope that it will be useful,
+/// but WITHOUT ANY WARRANTY; without even the implied warranty of
+/// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.See the
+/// GNU General Public License for more details.
+///
+/// You should have received a copy of the GNU General Public License
+/// along with Foobar.If not, see <http://www.gnu.org/licenses/>.
+///
+
+#ifndef INC_ARUDE_TEST_HPP
+#define INC_ARUDE_TEST_HPP
+
+#include <boost/test/unit_test.hpp>
+
+#endif // #ifndef INC_ARUDE_TEST_HPP
